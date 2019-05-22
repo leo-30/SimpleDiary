ed40aaea836011c257010bef936037e2ee254135
leo_rabbit30 <mio.rabbit971.snow@gmail.com>
2019-05-22 14:38:00 +0900

    Initial Commit

---
+++ SimpleDiary_LiT!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleDiary_LiT!
+//
+//  Created by 原田澪 on 2019/05/22.
+//  Copyright © 2019 原田澪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
