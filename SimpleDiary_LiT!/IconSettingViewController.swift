665c6a10fbfe71416df660f949391ec0cf1bc347
leo_rabbit30 <mio.rabbit971.snow@gmail.com>
2019-05-24 16:24:02 +0900

    アイコンの登録画面変更

--- SimpleDiary_LiT!/IconSettingViewController.swift
+++ SimpleDiary_LiT!/IconSettingViewController.swift
@@ -11,58 +11,20 @@ import RealmSwift
 
 class IconSettingViewController: UIViewController {
     
-    @IBOutlet var contextTextView: UITextView!
-    @IBOutlet var icon1ImageView: UIImageView!
-    @IBOutlet var icon2ImageView: UIImageView!
-    @IBOutlet var icon3ImageView: UIImageView!
-    @IBOutlet var icon4ImageView: UIImageView!
-    @IBOutlet var icon5ImageView: UIImageView!
-    @IBOutlet var icon6ImageView: UIImageView!
+    @IBOutlet var contextTextView1: UITextView!
+    @IBOutlet var contextTextView2: UITextView!
+    @IBOutlet var contextTextView3: UITextView!
+    @IBOutlet var contextTextView4: UITextView!
+    @IBOutlet var contextTextView5: UITextView!
+    @IBOutlet var contextTextView6: UITextView!
     
-    var iconInfo1: String!
-    var iconInfo2: String!
-    var iconInfo3: String!
-    var iconInfo4: String!
-    var iconInfo5: String!
-    var iconInfo6: String!
-    
-    var iconImage1: [UIImage]!
-    var iconImage2: [UIImage]!
-    var iconImage3: [UIImage]!
-    var iconImage4: [UIImage]!
-    var iconImage5: [UIImage]!
-    var iconImage6: [UIImage]!
-    
-    var iconNumber = 0
+    @IBAction func tapToSetting(_ sender: Any) {
+        self.performSegue(withIdentifier: "iconToSetting", sender: nil)
+    }
     
     //textField以外の部分をタップするとキーボードをしまう
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
-    }
-    
-    @IBAction func tapIcon1() {
-        iconNumber = 1
-        icon1ImageView.image = iconImage1[1]
-    }
-    @IBAction func tapIcon2() {
-        iconNumber = 2
-        icon2ImageView.image = iconImage2[1]
-    }
-    @IBAction func tapIcon3() {
-        iconNumber = 3
-        icon3ImageView.image = iconImage3[1]
-    }
-    @IBAction func tapIcon4() {
-        iconNumber = 4
-        icon4ImageView.image = iconImage4[1]
-    }
-    @IBAction func tapIcon5() {
-        iconNumber = 5
-        icon5ImageView.image = iconImage5[1]
-    }
-    @IBAction func tapIcon6() {
-        iconNumber = 6
-        icon6ImageView.image = iconImage6[1]
     }
     
     @IBAction func tapSave(_ sender: UIButton) {
@@ -74,54 +36,12 @@ class IconSettingViewController: UIViewController {
         
         //STEP.2 保存する要素を書く
         let icon = Icon()
-        if iconNumber == 1 {
-            icon.iconInfo1 = contextTextView.text
-            icon.iconInfo2 = iconInfo2
-            icon.iconInfo3 = iconInfo3
-            icon.iconInfo4 = iconInfo4
-            icon.iconInfo5 = iconInfo5
-            icon.iconInfo6 = iconInfo6
-        }
-        if iconNumber == 2 {
-            icon.iconInfo2 = contextTextView.text
-            icon.iconInfo1 = iconInfo1
-            icon.iconInfo3 = iconInfo3
-            icon.iconInfo4 = iconInfo4
-            icon.iconInfo5 = iconInfo5
-            icon.iconInfo6 = iconInfo6
-        }
-        if iconNumber == 3 {
-            icon.iconInfo3 = contextTextView.text
-            icon.iconInfo1 = iconInfo1
-            icon.iconInfo2 = iconInfo2
-            icon.iconInfo4 = iconInfo4
-            icon.iconInfo5 = iconInfo5
-            icon.iconInfo6 = iconInfo6
-        }
-        if iconNumber == 4 {
-            icon.iconInfo4 = contextTextView.text
-            icon.iconInfo1 = iconInfo1
-            icon.iconInfo2 = iconInfo2
-            icon.iconInfo3 = iconInfo3
-            icon.iconInfo5 = iconInfo5
-            icon.iconInfo6 = iconInfo6
-        }
-        if iconNumber == 5 {
-            icon.iconInfo5 = contextTextView.text
-            icon.iconInfo1 = iconInfo1
-            icon.iconInfo2 = iconInfo2
-            icon.iconInfo3 = iconInfo3
-            icon.iconInfo4 = iconInfo4
-            icon.iconInfo6 = iconInfo6
-        }
-        if iconNumber == 6 {
-            icon.iconInfo6 = contextTextView.text
-            icon.iconInfo1 = iconInfo1
-            icon.iconInfo2 = iconInfo2
-            icon.iconInfo3 = iconInfo3
-            icon.iconInfo4 = iconInfo4
-            icon.iconInfo5 = iconInfo5
-        }
+        icon.iconInfo1 = contextTextView1.text
+        icon.iconInfo2 = contextTextView2.text
+        icon.iconInfo3 = contextTextView3.text
+        icon.iconInfo4 = contextTextView4.text
+        icon.iconInfo5 = contextTextView5.text
+        icon.iconInfo6 = contextTextView6.text
         
         //STEP.3 Realmに書き込み
         try! realm.write {
@@ -133,17 +53,6 @@ class IconSettingViewController: UIViewController {
         
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        iconImage1 = [UIImage(named: "flower1.png")!, UIImage(named: "flower2.png")!]
-        iconImage2 = [UIImage(named: "music1.png")!, UIImage(named: "music2.png")!]
-        iconImage3 = [UIImage(named: "good1.png")!, UIImage(named: "good2.png")!]
-        iconImage4 = [UIImage(named: "study1.png")!, UIImage(named: "study2.png")!]
-        iconImage5 = [UIImage(named: "book1.png")!, UIImage(named: "book2.png")!]
-        iconImage6 = [UIImage(named: "exercise1.png")!, UIImage(named: "exercise2.png")!]
-    }
-    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -151,24 +60,28 @@ class IconSettingViewController: UIViewController {
             let realm = try! Realm()
             
             if let savedIcon = realm.objects(Icon.self).last {
-                self.iconInfo1 = savedIcon.iconInfo1
-                self.iconInfo2 = savedIcon.iconInfo2
-                self.iconInfo3 = savedIcon.iconInfo3
-                self.iconInfo4 = savedIcon.iconInfo4
-                self.iconInfo5 = savedIcon.iconInfo5
-                self.iconInfo6 = savedIcon.iconInfo6
+                let iconInfo1 = savedIcon.iconInfo1
+                let iconInfo2 = savedIcon.iconInfo2
+                let iconInfo3 = savedIcon.iconInfo3
+                let iconInfo4 = savedIcon.iconInfo4
+                let iconInfo5 = savedIcon.iconInfo5
+                let iconInfo6 = savedIcon.iconInfo6
                 
-//                DispatchQueue.main.async {
-//                    self.contextTextView1.text = iconInfo1
-//                    self.contextTextView2.text = iconInfo2
-//                    self.contextTextView3.text = iconInfo3
-//                    self.contextTextView4.text = iconInfo4
-//                    self.contextTextView5.text = iconInfo5
-//                    self.contextTextView6.text = iconInfo6
-//                }
-            } else {
-                
+                DispatchQueue.main.async {
+                    self.contextTextView1.text = iconInfo1
+                    self.contextTextView2.text = iconInfo2
+                    self.contextTextView3.text = iconInfo3
+                    self.contextTextView4.text = iconInfo4
+                    self.contextTextView5.text = iconInfo5
+                    self.contextTextView6.text = iconInfo6
+                }
             }
         }
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
+
+
